92f59773dd40917f45052b5fc2188899fb364a07
Morabet <ismailelmorabet2@gmail.com>
2024-04-25 14:51:37 +0100

    fixed

--- alx-backend-storage/0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ alx-backend-storage/0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -10,19 +10,21 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
 BEGIN
     DECLARE sum_of_fac INT;
     DECLARE weighted_score DECIMAL(10, 2);
-
+    DECLARE average DECIMAL(10, 2);
     -- Calculate the sum of weights for projects related to the user
     SELECT SUM(weight) INTO sum_of_fac 
     FROM projects 
     WHERE id IN (SELECT project_id FROM corrections WHERE user_id = user_id);
 
     -- Calculate the weighted score for the user
-    SELECT SUM(weight * score) / sum_of_fac INTO weighted_score 
-    FROM projects
-    JOIN corrections ON projects.id = corrections.project_id
-    WHERE corrections.user_id = user_id;
+    SELECT SUM(weight * score) INTO weighted_score 
+    FROM projects, corrections 
+    WHERE projects.id = corrections.project_id
+    AND corrections.user_id = user_id;
 
-    UPDATE users SET average_score = weighted_score WHERE id = user_id;
+    SET average = weighted_score / sum_of_fac;
+
+    UPDATE users SET average_score = average WHERE id = user_id;
     
     -- Return the computed weighted score
     SELECT weighted_score AS Message;
--- alx-backend-storage/0x00-MySQL_Advanced/7-average_score.sql
+++ alx-backend-storage/0x00-MySQL_Advanced/7-average_score.sql
@@ -2,7 +2,7 @@
 -- computes and stores the average score for a student.
 
 DELIMITER //
-
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
@@ -10,7 +10,8 @@ BEGIN
 
 DECLARE user_score DECIMAL(10, 2);
 
-SELECT AVG(score) INTO user_score FROM corrections WHERE user_id = user_id;
+-- SELECT AVG(score) INTO user_score FROM corrections WHERE user_id = user_id;
+SELECT SUM(score) / COUNT(*) INTO user_score FROM corrections WHERE corrections.user_id = user_id;
 
 UPDATE users SET average_score = user_score WHERE id = user_id;
 
