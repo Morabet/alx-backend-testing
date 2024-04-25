e8781415e73b516f06198b959ca7789b1caa6cfd
Morabet <ismailelmorabet2@gmail.com>
2024-04-25 14:52:13 +0100

    fixed 100

--- alx-backend-storage/0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ alx-backend-storage/0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,33 +1,41 @@
--- creates a stored procedure ComputeAverageWeightedScoreForUser that
--- computes and store the average weighted score for a student.
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
 
-DELIMITER //
+-- Procedure ComputeAverageScoreForUser is taking 1 input:
+-- 		user_id, a users.id value (you can assume user_id is linked to an existing users)
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser; 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE sum_of_fac INT;
-    DECLARE weighted_score DECIMAL(10, 2);
-    DECLARE average DECIMAL(10, 2);
-    -- Calculate the sum of weights for projects related to the user
-    SELECT SUM(weight) INTO sum_of_fac 
-    FROM projects 
-    WHERE id IN (SELECT project_id FROM corrections WHERE user_id = user_id);
 
-    -- Calculate the weighted score for the user
-    SELECT SUM(weight * score) INTO weighted_score 
-    FROM projects, corrections 
-    WHERE projects.id = corrections.project_id
-    AND corrections.user_id = user_id;
+DECLARE sum_of_factors INT DEFAULT 0;
+DECLARE weighted_scores INT DEFAULT 0; 
+DECLARE average FLOAT DEFAULT 0;
 
-    SET average = weighted_score / sum_of_fac;
+SELECT sum(weight) INTO sum_of_factors FROM projects
+WHERE id IN
+(SELECT project_id FROM corrections WHERE user_id = user_id);
 
-    UPDATE users SET average_score = average WHERE id = user_id;
-    
-    -- Return the computed weighted score
-    SELECT weighted_score AS Message;
-END //
+-- print sum_of_factors
+-- SELECT sum_of_factors AS MESSAGE;
+
+SELECT sum(weight * score) INTO weighted_scores
+FROM projects, corrections
+WHERE
+projects.id = corrections.project_id
+AND
+corrections.user_id = user_id;
+
+-- print weighted_scores
+-- SELECT weighted_scores AS MESSAGE;
+
+SET average = weighted_scores / sum_of_factors;
+
+-- -- updating the users score
+UPDATE users SET average_score = average WHERE id = user_id;
+
+END $$
 
 DELIMITER ;
