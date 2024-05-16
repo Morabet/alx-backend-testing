b388863222fd8d79615aad64489d1b48b1de2b0d
Morabet <ismailelmorabet2@gmail.com>
2024-05-16 23:15:54 +0100

    all tasks

--- alx-backend-user-data/0x00-personal_data/filtered_logger.py
+++ alx-backend-user-data/0x00-personal_data/filtered_logger.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 """ Regex-ing"""
 
-from typing import List
-import re
+
 import logging
 import os
 import mysql.connector
+import re
+from typing import List
 
 
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str
-) -> str:
-    """ returns the log message obfuscated"""
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''returns the log message obfuscated:'''
     pattern = "|".join(fields)
     return re.sub(
         fr'({pattern})=([^{separator}]+)', f'\\1={redaction}', message
@@ -20,29 +23,30 @@ def filter_datum(
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        '''initializing the instance'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        filtered_message = filter_datum(self.fields, self.REDACTION,
-                                        record.getMessage(), self.SEPARATOR)
-
+        '''formatting record'''
+        filtered_message = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
         record.msg = filtered_message
         return super().format(record)
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
 def get_logger() -> logging.Logger:
-    """ Create logger """
+    """
+    the use of this function is to create a logging object
+    and add to it our custom formatter
+    """
 
     # Create a logger named "user_data"
     logger = logging.getLogger('user_data')
@@ -66,16 +70,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connect to the MySQL database and return the connection object."""
 
     connection = mysql.connector.connect(
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        database=os.getenv("PERSONAL_DATA_DB_NAME")
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        port=3306,
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        database=os.getenv('PERSONAL_DATA_DB_NAME', '')
     )
     return connection
 
 
 def main() -> None:
-    """ """
+    """ main function"""
+
     logger = get_logger()
     db = get_db()
     cursor = db.cursor()
