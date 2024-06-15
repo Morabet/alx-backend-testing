74a19d8b7213841d2065bc1afbe8f21df4f463e2
Morabet <ismailelmorabet2@gmail.com>
2024-06-15 15:25:31 +0100

    task 0 to 4

---
+++ alx-files_manager/controllers/UsersController.js
@@ -0,0 +1,27 @@
+import dbClient from '../utils/db' 
+import sha1 from 'sha1'
+
+class UsersController{
+
+    static async postNew(req, res) {
+        const {email, password} = req.body
+        const userCollection = dbClient.client.db().collection('users')
+        if (!email) {
+            res.status(400).json({ "error": "Missing email" })
+        }
+        else if (!password) {
+            res.status(400).json({ "error": "Missing password" })
+        }
+        else {
+            const user = await userCollection.findOne({email})
+            if (user)
+                res.status(400).json({ "error": "Already exist" })
+            else {
+                const result = await userCollection.insertOne({ email, password: sha1(password) })
+                res.status(201).json({id: result.insertedId, email})
+            }
+        }
+    }
+}
+
+export default UsersController
